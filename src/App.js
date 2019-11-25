e687fe6791aab60451db9e6dc293c4f856551aa1
James Smith <james@floppy.org.uk>
2019-11-25 21:14:14 +0000

    remove default css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Model model={{
+        "name": "Bob",
+        "type": "Hormagaunt",
+        "stats": {
+          "movement": 8,
+          "weapon_skill": 4,
+          "ballistic_skill": 4,
+          "wounds": 1
+        }
+      }}>
+      </Model>
     </div>
   );
 }
