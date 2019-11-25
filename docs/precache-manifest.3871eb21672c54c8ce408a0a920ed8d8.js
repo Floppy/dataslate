27eecd0f030202fb854ab1dde3b0c1a9e026e081
James Smith <james@floppy.org.uk>
2019-11-25 22:32:36 +0000

    add production build and symlink for gh-pages

---
+++ docs/precache-manifest.3871eb21672c54c8ce408a0a920ed8d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c18f1ad57f841555a34b3e1502017b",
+    "url": "/scriptorum/index.html"
+  },
+  {
+    "revision": "55048bc2722d58ad6516",
+    "url": "/scriptorum/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "aa613c17a0744b5e25ac",
+    "url": "/scriptorum/static/js/2.b5d05b4e.chunk.js"
+  },
+  {
+    "revision": "55048bc2722d58ad6516",
+    "url": "/scriptorum/static/js/main.1c5fb1c5.chunk.js"
+  },
+  {
+    "revision": "c14f34bbd12903de2cd4",
+    "url": "/scriptorum/static/js/runtime-main.e538d274.js"
+  }
+]);
