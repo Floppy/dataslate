96922f663567eb85bc34cc123ef0f0b0ec03196a
James Smith <james@floppy.org.uk>
2019-11-25 21:27:10 +0000

    handle multiple models

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import Model from './Model';
 
+const models = [
+  {
+    "name": "Deathleaper",
+    "type": "Lictor",
+    "stats": {
+      "movement": 9,
+      "weapon_skill": 2,
+      "ballistic_skill": 4,
+      "strength": 6,
+      "toughness": 4,
+      "wounds": 4,
+      "attacks": 3,
+      "leadership": 9,
+      "save": 5
+    },
+    "faction": "tyranids",
+    "keywords": ["infantry", "lictor", "kraken"]
+  },
+  {
+    "name": "Bob",
+    "type": "Hormagaunt",
+    "stats": {
+      "movement": 8,
+      "weapon_skill": 4,
+      "ballistic_skill": 4,
+      "strength": 3,
+      "toughness": 3,
+      "wounds": 1,
+      "attacks": 2,
+      "leadership": 5,
+      "save": 6
+    },
+    "faction": "tyranids",
+    "keywords": ["infantry", "hormagaunt", "kraken"]
+  }
+];
+
 function App() {
   return (
-    <div>
-      <Model model={{
-        "name": "Bob",
-        "type": "Hormagaunt",
-        "stats": {
-          "movement": 8,
-          "weapon_skill": 4,
-          "ballistic_skill": 4,
-          "strength": 3,
-          "toughness": 3,
-          "wounds": 1,
-          "attacks": 2,
-          "leadership": 5,
-          "save": 6
-        },
-        "faction": "tyranids",
-        "keywords": ["infantry", "hormagaunt", "kraken"]
-      }}>
-      </Model>
-    </div>
+    <>
+      {
+        models.map((model) => (
+          <Model model={model}>
+          </Model>
+        ))
+      }
+    </>
   );
 }
 
