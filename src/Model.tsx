dac81c80ae64cec54523a3dbb347c34ffadea833
James Smith <james@floppy.org.uk>
2019-11-29 08:42:40 +0000

    typescriptify model component

---
+++ src/Model.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import StatsTable from './StatsTable';
+
+type Props = {
+  model: {
+    type: string,
+    name: string,
+    stats: {
+      movement: number,
+      weapon_skill: number,
+      ballistic_skill: number,
+      strength: number,
+      toughness: number,
+      wounds: number,
+      attacks: number,
+      leadership: number,
+      save: number,
+    },
+    faction: string,
+  },
+};
+
+function Model(props: Props ) {
+  return (
+    <div className="model">
+      <h1>
+      {props.model.type}: {props.model.name}
+      </h1>
+      <StatsTable {...props.model.stats}/>
+      <div>
+        Keywords: {props.model.faction}
+      </div>
+    </div>
+  );
+}
+
+export default Model;
