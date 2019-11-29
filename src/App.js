0bca848ea7514b02f14c918b35b7c5e14d8a9281
James Smith <james@floppy.org.uk>
2019-11-29 00:20:53 +0000

    parse uploaded files

--- src/App.js
+++ src/App.js
@@ -1,63 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Model from './Model';
 import Dropzone from 'react-dropzone';
-var xpath = require('xpath')
-  , dom = require('xmldom').DOMParser
+const dom = require('xmldom').DOMParser;
+const xpath = require('xpath').useNamespaces({"roster": "http://www.battlescribe.net/schema/rosterSchema"});
 
-const models = [
-  {
-    "name": "Deathleaper",
-    "type": "Lictor",
-    "stats": {
-      "movement": 9,
-      "weapon_skill": 2,
-      "ballistic_skill": 4,
-      "strength": 6,
-      "toughness": 4,
-      "wounds": 4,
-      "attacks": 3,
-      "leadership": 9,
-      "save": 5
-    },
-    "faction": "tyranids",
-    "keywords": ["infantry", "lictor", "kraken"]
-  },
-  {
-    "name": "Bob",
-    "type": "Hormagaunt",
-    "stats": {
-      "movement": 8,
-      "weapon_skill": 4,
-      "ballistic_skill": 4,
-      "strength": 3,
-      "toughness": 3,
-      "wounds": 1,
-      "attacks": 2,
-      "leadership": 5,
-      "save": 6
-    },
-    "faction": "tyranids",
-    "keywords": ["infantry", "hormagaunt", "kraken"]
+const stat = (name, model) => {
+  const nodes = xpath(`roster:profiles/roster:profile[@typeName='Model']//roster:characteristic[@name='${name}']`, model);
+  if (nodes.length > 0) {
+    return parseInt(nodes[0].childNodes[0].nodeValue);
   }
-];
+  else
+    return "";
+};
 
-function App() {
-  return (
-    <>
-      <Dropzone onDrop={acceptedFiles => {
-        acceptedFiles.forEach((file) => {
-          const reader = new FileReader()
-          reader.onabort = () => console.log('file reading was aborted')
-          reader.onerror = () => console.log('file reading has failed')
-          reader.onload = () => {
-            console.log(reader.result);
-            var doc = new dom().parseFromString(reader.result);
-            var nodes = xpath.select("//force", doc);
-            console.log(nodes);
-          }
-          reader.readAsText(file)
-        })}
-      }>
+const parseModel = (model) => ({
+  name: model.getAttribute('customName'),
+  type: model.getAttribute('name'),
+  stats: {
+    movement: stat("M", model),
+    weapon_skill: stat("WS", model),
+    ballistic_skill: stat("BS", model),
+    strength: stat("S", model),
+    toughness: stat("T", model),
+    wounds: stat("W", model),
+    attacks: stat("A", model),
+    leadership: stat("Ld", model),
+    save: stat("Sv", model),
+  },
+  faction: "tyranids",
+  keywords: ["infantry", "lictor", "kraken"],
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      models: []
+    };
+  }
+
+  parseFile = (event) => {
+    var models = [];
+    var doc = new dom().parseFromString(event.target.result);
+    for (const category of xpath("//roster:force/roster:categories/roster:category", doc)) {
+      const category_id = category.getAttribute('entryId')
+      for (const model of xpath(`//roster:selection[roster:categories/roster:category/@entryId='${category_id}']`, doc)) {
+        models.push(parseModel(model));
+      }
+    }
+    this.setState({models});
+  };
+
+  fileHandler = (acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onloadend = this.parseFile;
+      reader.readAsText(file)
+    })
+  };
+
+  render() {
+    console.log(this.state);
+    return (<>
+      <Dropzone onDrop={this.fileHandler}>
         {({getRootProps, getInputProps}) => (
           <section>
             <div {...getRootProps()}>
@@ -68,13 +75,13 @@ function App() {
         )}
       </Dropzone>
       {
-        models.map((model) => (
+        this.state.models.map((model) => (
           <Model model={model}>
           </Model>
         ))
       }
-    </>
-  );
+    </>);
+  }
 }
 
 export default App;
