c9586279258bc1050ddeafebf5a02d81863d5aee
James Smith <james@floppy.org.uk>
2019-12-12 09:11:21 +0000

    standard js fixes

--- src/App.js
+++ src/App.js
@@ -1,144 +1,140 @@
-import React from 'react';
-import Model from './Model';
-import Dropzone from 'react-dropzone';
-const pluralize = require('pluralize');
-const _ = require('lodash');
-const hash = require('node-object-hash')([]);
-const dom = require('xmldom').DOMParser;
-const xpath = require('xpath').useNamespaces({"roster": "http://www.battlescribe.net/schema/rosterSchema"});
+import React from 'react'
+import Model from './Model'
+import Dropzone from 'react-dropzone'
+const _ = require('lodash')
+const hash = require('node-object-hash')([])
+const DOMParser = require('xmldom').DOMParser
+const xpath = require('xpath').useNamespaces({ roster: 'http://www.battlescribe.net/schema/rosterSchema' })
 
 const stat = (name, model) => {
-  const nodes = xpath(`roster:profiles/roster:profile[@typeName='Model']//roster:characteristic[@name='${name}']`, model);
+  const nodes = xpath(`roster:profiles/roster:profile[@typeName='Model']//roster:characteristic[@name='${name}']`, model)
   if (nodes.length > 0) {
-    return parseInt(nodes[0].childNodes[0].nodeValue);
-  }
-  else
-    return "";
-};
+    return parseInt(nodes[0].childNodes[0].nodeValue)
+  } else { return '' }
+}
 
 const weaponStat = (name, weapon) => {
-  const nodes = xpath(`roster:characteristics/roster:characteristic[@name='${name}']`, weapon);
+  const nodes = xpath(`roster:characteristics/roster:characteristic[@name='${name}']`, weapon)
   if (nodes.length > 0) {
-    return parseInt(nodes[0].childNodes[0].nodeValue);
-  }
-  else
-    return "";
-};
+    return parseInt(nodes[0].childNodes[0].nodeValue)
+  } else { return '' }
+}
 
 const parseAbility = (ability) => {
-  const description = xpath("roster:characteristics/roster:characteristic[@name='Description']", ability)[0].childNodes[0].nodeValue;
-  let phases = [];
+  const description = xpath("roster:characteristics/roster:characteristic[@name='Description']", ability)[0].childNodes[0].nodeValue
+  const phases = []
   switch (true) {
     case /charge/i.test(description):
-      phases.push('movement');
-      break;
+      phases.push('movement')
+      break
     case /advance/i.test(description):
-      phases.push('movement');
-      break;
+      phases.push('movement')
+      break
     case /move/i.test(description):
-      phases.push('movement');
-      break;
+      phases.push('movement')
+      break
     case /psychic/i.test(description):
-      phases.push('psychic');
-      break;
+      phases.push('psychic')
+      break
     case /shoot/i.test(description):
-      phases.push('shooting');
-      break;
+      phases.push('shooting')
+      break
     case /nerve/i.test(description):
-      phases.push('morale');
-      break;
+      phases.push('morale')
+      break
     default:
-      phases.push('all');
+      phases.push('all')
   }
   return {
     name: ability.getAttribute('name'),
     description,
-    phases,
+    phases
   }
-};
+}
 
 const parseWeapon = (weapon) => ({
   name: weapon.getAttribute('name'),
-  range: weaponStat("Range", weapon),
-  strength: weaponStat("S", weapon),
-});
+  range: weaponStat('Range', weapon),
+  strength: weaponStat('S', weapon)
+})
 
 const parseModel = (model) => {
-  const abilities = xpath("roster:profiles/roster:profile[@typeName='Ability']", model).map(parseAbility);
-  const weapons = xpath("roster:selections/roster:selection/roster:profiles/roster:profile[@typeName='Weapon']", model).map(parseWeapon);
+  const abilities = xpath("roster:profiles/roster:profile[@typeName='Ability']", model).map(parseAbility)
+  const weapons = xpath("roster:selections/roster:selection/roster:profiles/roster:profile[@typeName='Weapon']", model).map(parseWeapon)
   return {
     name: model.getAttribute('customName'),
     type: model.getAttribute('name'),
     category: xpath("roster:categories/roster:category[@primary='true']", model)[0].getAttribute('name'),
     stats: {
-      movement: stat("M", model),
-      weapon_skill: stat("WS", model),
-      ballistic_skill: stat("BS", model),
-      strength: stat("S", model),
-      toughness: stat("T", model),
-      wounds: stat("W", model),
-      attacks: stat("A", model),
-      leadership: stat("Ld", model),
-      save: stat("Sv", model),
+      movement: stat('M', model),
+      weapon_skill: stat('WS', model),
+      ballistic_skill: stat('BS', model),
+      strength: stat('S', model),
+      toughness: stat('T', model),
+      wounds: stat('W', model),
+      attacks: stat('A', model),
+      leadership: stat('Ld', model),
+      save: stat('Sv', model)
     },
     abilities,
     weapons,
     faction: xpath("roster:categories/roster:category[@primary='false' and starts-with(@name,'Faction: ')]", model)[0].getAttribute('name'),
-    keywords: xpath("roster:categories/roster:category[@primary='false' and not(starts-with(@name,'Faction: '))]", model).map((x) => x.getAttribute('name')),
-  };
-};
+    keywords: xpath("roster:categories/roster:category[@primary='false' and not(starts-with(@name,'Faction: '))]", model).map((x) => x.getAttribute('name'))
+  }
+}
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       models: []
-    };
+    }
   }
 
   parseFile = (event) => {
-    var models = [];
-    var doc = new dom().parseFromString(event.target.result);
-    for (const category of xpath("//roster:force/roster:categories/roster:category", doc)) {
-      const categoryId = category.getAttribute('entryId');
+    var models = []
+    var doc = new DOMParser().parseFromString(event.target.result)
+    for (const category of xpath('//roster:force/roster:categories/roster:category', doc)) {
+      const categoryId = category.getAttribute('entryId')
       for (const model of xpath(`//roster:selection[@type='model' and roster:categories/roster:category/@entryId='${categoryId}']`, doc)) {
-        models.push(parseModel(model));
+        models.push(parseModel(model))
       }
     }
-    this.setState({models: _.uniqBy(models, hash.hash)});
+    this.setState({ models: _.uniqBy(models, hash.hash) })
   };
 
-  fileHandler = (acceptedFiles) => {
+  handleDrop = (acceptedFiles) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader()
       reader.onabort = () => console.log('file reading was aborted')
       reader.onerror = () => console.log('file reading has failed')
-      reader.onloadend = this.parseFile;
+      reader.onloadend = this.parseFile
       reader.readAsText(file)
     })
   };
 
-  render() {
-    return (<>
-      <Dropzone onDrop={this.fileHandler}>
-        {({getRootProps, getInputProps}) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drop a .ros file here, or click to select one</p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
-      {
-        // Display models sorted by category and type
-        _.sortBy(this.state.models, (x) => ([x.category == null, x.category, x.type])).map((model) => (
-          <Model model={model}>
-          </Model>
-        ))
-      }
-    </>);
+  render () {
+    return (
+      <>
+        <Dropzone onDrop={this.handleDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drop a .ros file here, or click to select one</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        {
+          // Display models sorted by category and type
+          _.sortBy(this.state.models, (x) => ([x.category == null, x.category, x.type])).map((model) => (
+            <Model model={model} key={hash.hash(model)} />
+          ))
+        }
+      </>
+    )
   }
 }
 
-export default App;
+export default App
--- src/StatsTable.js
+++ src/StatsTable.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React from 'react'
 
-function StatsTable(props) {
+function StatsTable (props) {
   return (
     <table>
-    <tr>
-      <th>{"M"}</th>
-      <th>{"WS"}</th>
-      <th>{"BS"}</th>
-      <th>{"S"}</th>
-      <th>{"T"}</th>
-      <th>{"W"}</th>
-      <th>{"A"}</th>
-      <th>{"Ld"}</th>
-      <th>{"Sv"}</th>
-    </tr>
-    <tr>
-      <td>{props.movement}&quot;</td>
-      <td>{props.weapon_skill}+</td>
-      <td>{props.ballistic_skill}+</td>
-      <td>{props.strength}</td>
-      <td>{props.toughness}</td>
-      <td>{props.wounds}</td>
-      <td>{props.attacks}</td>
-      <td>{props.leadership}</td>
-      <td>{props.save}+</td>
-    </tr>
-  </table>
-  );
+      <tr>
+        <th>M</th>
+        <th>WS</th>
+        <th>BS</th>
+        <th>S</th>
+        <th>T</th>
+        <th>W</th>
+        <th>A</th>
+        <th>Ld</th>
+        <th>Sv</th>
+      </tr>
+      <tr>
+        <td>{props.movement}&quot;</td>
+        <td>{props.weapon_skill}+</td>
+        <td>{props.ballistic_skill}+</td>
+        <td>{props.strength}</td>
+        <td>{props.toughness}</td>
+        <td>{props.wounds}</td>
+        <td>{props.attacks}</td>
+        <td>{props.leadership}</td>
+        <td>{props.save}+</td>
+      </tr>
+    </table>
+  )
 }
 
-export default StatsTable;
+export default StatsTable
