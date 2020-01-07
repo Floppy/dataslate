6341989bc3c15d0c3297ff3ea70b06d8f624faf1
James Smith <james@floppy.org.uk>
2020-01-07 23:51:11 +0000

    Add stats badges at the side

--- src/components/Datasheet.tsx
+++ src/components/Datasheet.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import StatsTable from './StatsTable';
+import StatBadge from './StatBadge';
 import AbilityList from './AbilityList';
 import PsychicPowerList from './PsychicPowerList';
 import WeaponList from './WeaponList';
 import Phase from './Phase';
 import { SpecialistIcon } from './SpecialistIcon';
 import { CategoryIcon } from './CategoryIcon';
-import { Badge } from 'react-bootstrap'
+import { Badge, Row, Col } from 'react-bootstrap'
 
 import { Model } from '../types';
 
@@ -64,26 +65,51 @@ function Datasheet(props: Props) {
       <StatsTable {...props.model.stats}/>
       <AbilityList abilities={props.model.abilities} phase=''/>
       <Phase name="movement">
-        <p className="lead">Movement: {props.model.stats.movement}&quot;</p>
-        <AbilityList abilities={props.model.abilities} phase='movement'/>
+        <Row>
+          <Col sm='1'>
+            <StatBadge name="M" value={`${props.model.stats.movement}"`}/>
+          </Col>
+          <Col>
+            <AbilityList abilities={props.model.abilities} phase='movement'/>
+          </Col>
+        </Row>
       </Phase>
       <Phase name="psychic">
         <PsychicPowerList powers={props.model.psychicPowers}/>
         <AbilityList abilities={props.model.abilities} phase='psychic'/>
       </Phase>
       <Phase name="shooting">
-        <p className="lead">Ballistic Skill: {props.model.stats.ballistic_skill}+</p>
-        <WeaponList weapons={props.model.weapons} phase='shooting' userStrength={props.model.stats.strength}/>
-        <AbilityList abilities={props.model.abilities} phase='shooting'/>
+        <Row>
+          <Col sm='1'>
+            <StatBadge name="BS" value={`${props.model.stats.ballistic_skill}+`}/>
+          </Col>
+          <Col>
+            <WeaponList weapons={props.model.weapons} phase='shooting' userStrength={props.model.stats.strength}/>
+            <AbilityList abilities={props.model.abilities} phase='shooting'/>
+          </Col>
+        </Row>
       </Phase>
       <Phase name="fight">
-        <p className="lead">Attacks: {props.model.stats.attacks}, Weapon Skill: {props.model.stats.weapon_skill}+</p>
-        <WeaponList weapons={props.model.weapons} phase='fight' userStrength={props.model.stats.strength}/>
-        <AbilityList abilities={props.model.abilities} phase='fight'/>
+        <Row>
+          <Col sm='1'>
+            <StatBadge name="A" value={`${props.model.stats.attacks}`}/>
+            <StatBadge name="WS" value={`${props.model.stats.weapon_skill}+`}/>
+          </Col>
+          <Col>
+            <WeaponList weapons={props.model.weapons} phase='fight' userStrength={props.model.stats.strength}/>
+            <AbilityList abilities={props.model.abilities} phase='fight'/>
+          </Col>
+        </Row>
       </Phase>
       <Phase name="morale">
-        <p className="lead">Leadership: {props.model.stats.leadership}</p>
+        <Row>
+          <Col sm='1'>
+            <StatBadge name="Ld" value={`${props.model.stats.leadership}`}/>
+          </Col>
+          <Col>
         <AbilityList abilities={props.model.abilities} phase='morale'/>
+           </Col>
+        </Row>
       </Phase>
     </div>
   );
--- src/components/StatsTable.tsx
+++ src/components/StatsTable.tsx
@@ -1,34 +1,45 @@
 import React from 'react'
-import { ModelStats } from '../types'
 import { Table } from 'react-bootstrap'
 
-function StatsTable (props: ModelStats) {
+type Props = {
+  movement?: number,
+  weapon_skill?: number,
+  ballistic_skill?: number,
+  strength?: number,
+  toughness?: number,
+  wounds?: number,
+  attacks?: number,
+  leadership?: number,
+  save?: number,
+}
+
+function StatsTable (props: Props) {
   return (
     <Table striped bordered>
       <thead>
         <tr>
-          <th>M</th>
-          <th>WS</th>
-          <th>BS</th>
-          <th>S</th>
-          <th>T</th>
-          <th>W</th>
-          <th>A</th>
-          <th>Ld</th>
-          <th>Sv</th>
+          {props.movement ? <th>Movement</th> : null}
+          {props.weapon_skill ? <th>Weapon Skill</th> : null}
+          {props.ballistic_skill ? <th>Ballistic Skill</th> : null}
+          {props.strength ? <th>Strength</th> : null}
+          {props.toughness ? <th>Toughness</th> : null}
+          {props.wounds ? <th>Wounds</th> : null}
+          {props.attacks ? <th>Attacks</th> : null}
+          {props.leadership ? <th>Leadership</th> : null}
+          {props.save ? <th>Save</th> : null}
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>{props.movement}&quot;</td>
-          <td>{props.weapon_skill}+</td>
-          <td>{isNaN(props.ballistic_skill) ? '-' : props.ballistic_skill+"+"}</td>
-          <td>{props.strength}</td>
-          <td>{props.toughness}</td>
-          <td>{props.wounds}</td>
-          <td>{props.attacks}</td>
-          <td>{props.leadership}</td>
-          <td>{props.save}+</td>
+          {props.movement ? <td>{props.movement}&quot;</td> : null}
+          {props.weapon_skill ? <td>{props.weapon_skill}+</td> : null}
+          {props.ballistic_skill ? <td>{isNaN(props.ballistic_skill) ? '-' : props.ballistic_skill+"+"}</td> : null}
+          {props.strength ? <td>{props.strength}</td> : null}
+          {props.toughness ? <td>{props.toughness}</td> : null}
+          {props.wounds ? <td>{props.wounds}</td> : null}
+          {props.attacks ? <td>{props.attacks}</td> : null}
+          {props.leadership ? <td>{props.leadership}</td> : null}
+          {props.save ? <td>{props.save}+</td> : null}
         </tr>
       </tbody>
     </Table>
