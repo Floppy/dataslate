3d70a5f92869d24ae646e9d75745b81cdccb19cf
James Smith <james@floppy.org.uk>
2020-01-10 17:44:03 +0000

    remove smaller stats tables

--- src/components/StatsTable.tsx
+++ src/components/StatsTable.tsx
@@ -2,44 +2,44 @@ import React from 'react'
 import { Table } from 'react-bootstrap'
 
 type Props = {
-  movement?: number,
-  weapon_skill?: number,
-  ballistic_skill?: number,
-  strength?: number,
-  toughness?: number,
-  wounds?: number,
-  attacks?: number,
-  leadership?: number,
-  save?: number,
+  movement: number,
+  weapon_skill: number,
+  ballistic_skill: number,
+  strength: number,
+  toughness: number,
+  wounds: number,
+  attacks: number,
+  leadership: number,
+  save: number,
 }
 
 function StatsTable (props: Props) {
   return (
-    <Table striped bordered>
+    <Table striped bordered size="sm">
       <thead>
         <tr>
-          {props.movement ? <th>Movement</th> : null}
-          {props.weapon_skill ? <th>Weapon Skill</th> : null}
-          {props.ballistic_skill ? <th>Ballistic Skill</th> : null}
-          {props.strength ? <th>Strength</th> : null}
-          {props.toughness ? <th>Toughness</th> : null}
-          {props.wounds ? <th>Wounds</th> : null}
-          {props.attacks ? <th>Attacks</th> : null}
-          {props.leadership ? <th>Leadership</th> : null}
-          {props.save ? <th>Save</th> : null}
+          <th>Movement</th>
+          <th>Weapon Skill</th>
+          <th>Ballistic Skill</th>
+          <th>Strength</th>
+          <th>Toughness</th>
+          <th>Wounds</th>
+          <th>Attacks</th>
+          <th>Leadership</th>
+          <th>Save</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          {props.movement ? <td>{props.movement}&quot;</td> : null}
-          {props.weapon_skill ? <td>{props.weapon_skill}+</td> : null}
-          {props.ballistic_skill ? <td>{isNaN(props.ballistic_skill) ? '-' : props.ballistic_skill+"+"}</td> : null}
-          {props.strength ? <td>{props.strength}</td> : null}
-          {props.toughness ? <td>{props.toughness}</td> : null}
-          {props.wounds ? <td>{props.wounds}</td> : null}
-          {props.attacks ? <td>{props.attacks}</td> : null}
-          {props.leadership ? <td>{props.leadership}</td> : null}
-          {props.save ? <td>{props.save}+</td> : null}
+          <td>{props.movement}&quot;</td>
+          <td>{props.weapon_skill}+</td>
+          <td>{isNaN(props.ballistic_skill) ? '-' : props.ballistic_skill+"+"}</td>
+          <td>{props.strength}</td>
+          <td>{props.toughness}</td>
+          <td>{props.wounds}</td>
+          <td>{props.attacks}</td>
+          <td>{props.leadership}</td>
+          <td>{props.save}+</td>
         </tr>
       </tbody>
     </Table>
