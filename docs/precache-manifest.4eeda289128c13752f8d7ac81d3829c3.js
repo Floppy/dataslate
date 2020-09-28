6f8462a5500797fd5c376e2c883fdfc103bbfa14
James Smith <james@floppy.org.uk>
2020-09-28 20:14:35 +0100

    production build

---
+++ docs/precache-manifest.4eeda289128c13752f8d7ac81d3829c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d5fd3ed26b9425c5e035b999d52e37",
+    "url": "/scriptorum/index.html"
+  },
+  {
+    "revision": "6110668b883990528756",
+    "url": "/scriptorum/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0f1d36686471c2b24ce8",
+    "url": "/scriptorum/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6110668b883990528756",
+    "url": "/scriptorum/static/js/2.61065bbf.chunk.js"
+  },
+  {
+    "revision": "5509b05e5b9c43b321283d108cdc882f",
+    "url": "/scriptorum/static/js/2.61065bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1d36686471c2b24ce8",
+    "url": "/scriptorum/static/js/main.f11abc26.chunk.js"
+  },
+  {
+    "revision": "ff84009b2e74ea52a881",
+    "url": "/scriptorum/static/js/runtime-main.1fb77578.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scriptorum/precache-manifest.d82217ae3d82e2f6d8d0d0d7a72eccb1.js"
+  "/scriptorum/precache-manifest.4eeda289128c13752f8d7ac81d3829c3.js"
 );
 
 self.addEventListener('message', (event) => {
