453784b34ca0c5d8270a9f4d7ac04fa6f9ce6a8e
James Smith <james@floppy.org.uk>
2020-10-19 17:42:10 +0100

    add scouting, initiative, and deployment phases as appropriate

---
+++ src/components/DeploymentPhaseDetails.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AbilityList from './AbilityList';
+import { Row, Col } from 'react-bootstrap'
+import { ModelTitle } from './ModelTitle';
+import { Model } from '../types';
+
+type Props = {
+  model: Model
+};
+
+export function DeploymentPhaseDetails(props: Props) {
+  return (
+    <Row>
+      <Col>
+        <ModelTitle model={props.model}/>
+        <AbilityList abilities={props.model.abilities} phase='deployment'/>
+      </Col>
+    </Row>
+  );
+}
+
+export function hasDeploymentPhase(model: Model) {
+  return ((model.abilities.filter((x) => (x.phases.indexOf("deployment") >= 0))).length > 0)
+}
---
+++ src/components/ScoutingPhaseDetails.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AbilityList from './AbilityList';
+import { Row, Col } from 'react-bootstrap'
+import { ModelTitle } from './ModelTitle';
+import { Model } from '../types';
+
+type Props = {
+  model: Model
+};
+
+export function ScoutingPhaseDetails(props: Props) {
+  return (
+    <Row>
+      <Col>
+        <ModelTitle model={props.model}/>
+        <AbilityList abilities={props.model.abilities} phase='scouting'/>
+      </Col>
+    </Row>
+  );
+}
+
+export function hasScoutingPhase(model: Model) {
+  return ((model.abilities.filter((x) => (x.phases.indexOf("scouting") >= 0))).length > 0)
+}
