99d8a21dc83404a2aea7262b924e3f80173926fe
James Smith <james@floppy.org.uk>
2020-10-20 19:41:39 +0100

    add sentry error reporting

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import * as Sentry from "@sentry/react";
+// import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://6fe3e632b45a44c086068e13f3b1a783@o464596.ingest.sentry.io/5473956",
+  // integrations: [
+  //   new Integrations.BrowserTracing(),
+  // ],
+  // tracesSampleRate: 0.0,
+});
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
