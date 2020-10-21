23fcbcf45eae6664af4e541b555bdd9b57ec5832
Floppy <james@floppy.org.uk>
2020-10-21 21:49:05 +0000

    Deploying to gh-pages from  @ ebde589c8283f801acad44e980a531303b0afa30 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dataslate/precache-manifest.2ecec72e8b3059634fad280b9ecddbb7.js"
+  "/precache-manifest.0ca73dc485e85e10c8156642cb2e0c18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dataslate/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
