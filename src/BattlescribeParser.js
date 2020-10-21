e0f6854e0d467c43d6f32235a31d22719fb53b6f
James Smith <james@floppy.org.uk>
2020-10-21 21:13:24 +0100

    lint

--- src/BattlescribeParser.js
+++ src/BattlescribeParser.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 const DOMParser = require('xmldom').DOMParser
 const xpath = require('xpath-ts').useNamespaces({ roster: 'http://www.battlescribe.net/schema/rosterSchema' })
 const _ = require('lodash')
