e1bdb826b0816a94ba97ca7d5686384b8833246d
James Smith <james@floppy.org.uk>
2020-10-21 22:12:38 +0100

    rename to dataslate

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -58,8 +58,8 @@ function Homepage(props: Props) {
     <footer>
       Built with <a href='https://reactjs.org/'>React</a> and <a href='https://getbootstrap.com'>Bootstrap</a>.
       Icons courtesy of <a href='https://killteam.app/'>Companion for Kill Team</a>.
-      Released as <a href='https://github.com/floppy/scriptorum'>Open Source</a>,
-      report problems <a href='https://github.com/floppy/scriptorum/issues/new'>on GitHub</a>.
+      Released as <a href='https://github.com/floppy/dataslate'>Open Source</a>,
+      report problems <a href='https://github.com/floppy/dataslate/issues/new'>on GitHub</a>.
     </footer>
   </>
 }
