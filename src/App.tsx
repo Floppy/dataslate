e21942fb145f8c5fb8268826876edaacc297e1db
James Smith <james@floppy.org.uk>
2020-10-25 22:16:30 +0000

    convert App to typescript

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Homepage from './components/Homepage'
+import Roster from './components/Roster'
+import { Container } from 'react-bootstrap'
+import { loadFiles } from './FileLoader'
+import { Model, Ability } from './types';
+
+export function App() {
+
+  const [name, setName] = useState<string>("")
+  const [models, setModels] = useState<Model[]>([])
+  const [forceRules, setForceRules] = useState<Ability[]>([])
+
+  const handleUpload = (acceptedFiles: any) => {
+    loadFiles(acceptedFiles).then(({name, forceRules, models}) => {
+      setName(name)
+      setForceRules(forceRules)
+      setModels(models)
+    })
+  }
+
+  const handleClose = () => {
+    setForceRules([])
+    setModels([])
+    setName("")
+  }
+
+  const handleSelectionChanged = (uuid: string, selectedCount: number) => (
+    setModels(models.map((model: Model) => (
+      model.uuid !== uuid
+        ? model
+        : {
+            ...model,
+            selected: selectedCount
+          }
+    )))
+  )
+
+  return (
+    <Container fluid='lg'>
+      {models.length === 0
+        ? <Homepage onUpload={handleUpload} />
+        : <Roster name={name} models={models} onClose={handleClose} forceRules={forceRules} onSelectionChanged={handleSelectionChanged} />}
+    </Container>
+  )
+}
