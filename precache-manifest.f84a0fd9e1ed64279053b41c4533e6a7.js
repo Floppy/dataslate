3dd5d558df4d03ca2d535a5e2479678b58ecf9ed
Floppy <james@floppy.org.uk>
2020-10-26 22:42:07 +0000

    Deploying to gh-pages from  @ e9c0f30dfb13f7ac191fd44f53ceee1eedc2783c 🚀

---
+++ precache-manifest.f84a0fd9e1ed64279053b41c4533e6a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950cd99220558f18b4cfe9eb121fb8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb58d66494e804feade",
+    "url": "/static/css/2.91e85c17.chunk.css"
+  },
+  {
+    "revision": "cabc4f185030c2bda2bb",
+    "url": "/static/css/main.9bd1e02f.chunk.css"
+  },
+  {
+    "revision": "7eb58d66494e804feade",
+    "url": "/static/js/2.59047194.chunk.js"
+  },
+  {
+    "revision": "e5dcebbc5a947f79dc58230e8176bb0b",
+    "url": "/static/js/2.59047194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cabc4f185030c2bda2bb",
+    "url": "/static/js/main.346500c0.chunk.js"
+  },
+  {
+    "revision": "5bf20bed22feaf5545a4",
+    "url": "/static/js/runtime-main.1215acaa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0a24d9f54899457d5b6bec27510609d.js"
+  "/precache-manifest.f84a0fd9e1ed64279053b41c4533e6a7.js"
 );
 
 self.addEventListener('message', (event) => {
