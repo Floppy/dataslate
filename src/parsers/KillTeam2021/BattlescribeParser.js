9f0464a0d5365d5cbadf9a558bab77eca764e75b
James Smith <james@floppy.org.uk>
2021-08-06 00:04:44 +0100

    tiny bit of KT2021 parsing

---
+++ src/parsers/KillTeam2021/BattlescribeParser.js
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from 'uuid'
+import _ from 'lodash'
+import * as XPath from 'xpath-ts'
+
+import hash from 'node-object-hash'
+
+// useNamespaces is NOT a React hook, so:
+// eslint-disable-next-line
+const xpath = XPath.useNamespaces({ roster: 'http://www.battlescribe.net/schema/rosterSchema' })
+
+const parseModel = (model) => {
+  const details = {
+    name: model.getAttribute('customName'),
+    type: model.getAttribute('name'),
+    category: null,
+    stats: null,
+    abilities: [],
+    weapons: [],
+    wargear: [],
+    psychicPowers: [],
+    specialism: null,
+    faction: null,
+    keywords: [],
+    points: 0
+  }
+  return { ...details, hash: hash([]).hash(details) }
+}
+
+export const parseBattlescribeXML = (doc) => {
+  console.log("KT2021 roster detected! Support coming soon, but nothing yet, sorry!");
+  const models = []
+  const name = xpath('/roster:roster', doc)[0].getAttribute('name')
+  for (const model of xpath(`//roster:selection[@type='model']`, doc)) {
+    models.push(parseModel(model))
+  }
+  return {
+    name,
+    forceRules: [],
+    models
+  }
+}
--- src/parsers/KillTeam2021/BattlescribeParser.ts
+++
@@ -1,3 +0,0 @@
-export const parseBattlescribeXML = (doc: any) : void => {
-  console.log("KT2021 roster detected! Support coming soon, but nothing yet, sorry!");
-}
