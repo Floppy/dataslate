5977d261d835ea420c71c5b9903f6fb82610e330
James Smith <james@floppy.org.uk>
2021-08-26 22:15:27 +0100

    parse stats for KT21

--- src/parsers/KillTeam2021/BattlescribeParser.ts
+++ src/parsers/KillTeam2021/BattlescribeParser.ts
@@ -1,5 +1,4 @@
 import * as XPath from 'xpath-ts'
-
 import hash from 'node-object-hash'
 import { Roster, Model } from '../../components/KillTeam2021/types';
 
@@ -7,30 +6,36 @@ import { Roster, Model } from '../../components/KillTeam2021/types';
 // eslint-disable-next-line
 const xpSelect = XPath.useNamespaces({ bs: 'http://www.battlescribe.net/schema/rosterSchema' })
 
+const stat = (name: string, model: Element) : number | null => {
+  const node = xpSelect(`bs:profiles/bs:profile[@typeName='Operative']//bs:characteristic[@name='M']/text()`, model, true)
+  if (node) {
+    return parseInt(node.toString())
+  } else { return null }
+}
+
+
 const parseModel = (model : Element) : Model => {
   const details = {
     name: xpSelect('string(@customName)', model, true).toString(),
     type: xpSelect('string(@name)', model, true).toString(),
     stats: {
-      movement: "3",
-      actionPointLimit: 2,
-      groupActivation: 1,
-      defence: 3,
-      save: 5,
+      movement: stat("M", model) + "⬤",
+      actionPointLimit: parseInt(xpSelect(`bs:profiles/bs:profile[@typeName='Operative']//bs:characteristic[@name='APL']/ text()`, model, true).toString()),
+      groupActivation: parseInt(xpSelect(`bs:profiles/bs:profile[@typeName='Operative']//bs:characteristic[@name='GA']/ text()`, model, true).toString()),
+      defence: parseInt(xpSelect(`bs:profiles/bs:profile[@typeName='Operative']//bs:characteristic[@name='DF']/ text()`, model, true).toString()),
+      save: parseInt(xpSelect(`bs:profiles/bs:profile[@typeName='Operative']//bs:characteristic[@name='SV']/ text()`, model, true).toString()),
       invulnerable_save: null,
-      wounds: 6,
+      wounds: parseInt(xpSelect(`bs:profiles/bs:profile[@typeName='Operative']//bs:characteristic[@name='W']/ text()`, model, true).toString()),
     },
-    faction: '',
-    keywords: [],
-    uuid: '',
-    count: 1,
+    keywords: (xpSelect("bs:categories/bs:category[@primary='false']/@name", model) as Node[]).map((x) => x.toString()),
+    uuid: xpSelect('string(@id)', model, true).toString(),
+    count: parseInt(xpSelect('string(@number)', model, true).toString()),
     selected: 1,
   };
   return { ...details, hash: hash().hash(details) }
 }
 
 export const parseBattlescribeXML = (doc : Document) : Roster => {
-  console.log('KT2021 roster detected! Support coming soon, but nothing yet, sorry!')
   const models = []
   const name = xpSelect('string(/bs:roster/@name)', doc, true).toString()
   for (const model of xpSelect('//bs:selection[@type=\'model\']', doc) as Element[]) {
