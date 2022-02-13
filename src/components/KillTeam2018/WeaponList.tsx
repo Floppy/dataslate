3c5fda16bf16096f653b87108dd9a460b4239c4c
James Smith <james@floppy.org.uk>
2022-02-13 15:06:25 +0000

    apply standard v11 lint rules
    mainly around ternaries

--- src/components/KillTeam2018/WeaponList.tsx
+++ src/components/KillTeam2018/WeaponList.tsx
@@ -14,36 +14,38 @@ function WeaponList (props: Props): JSX.Element {
     ? props.weapons.filter((x) => (x.type !== 'Melee'))
     : props.weapons.filter((x) => (x.type === 'Melee'))
 
-  return weapons.length === 0 ? <></> : (
-    <Table striped bordered size='sm'>
-      <thead>
-        <tr>
-          <th>Weapon</th>
-          {props.phase === 'shooting' && <th>Range</th>}
-          {props.phase === 'shooting' && <th>Type</th>}
-          {props.phase === 'shooting' && <th>Shots</th>}
-          <th>S</th>
-          <th>AP</th>
-          <th>D</th>
-          <th>Abilities</th>
-        </tr>
-      </thead>
-      <tbody>
-        {_.sortBy(weapons, ['name']).map((x: Weapon) => (
-          <tr key={x.id}>
-            <td style={{ whiteSpace: 'nowrap' }}>{x.name}</td>
-            {props.phase === 'shooting' && <td>{x.range}&quot;</td>}
-            {props.phase === 'shooting' && <td style={{ whiteSpace: 'nowrap' }}>{x.type}</td>}
-            {props.phase === 'shooting' && <td style={{ whiteSpace: 'nowrap' }}>{x.shots}</td>}
-            <td>{x.strength}</td>
-            <td>{x.armourPiercing}</td>
-            <td>{x.damage}</td>
-            <td>{x.abilities}</td>
+  return weapons.length === 0
+    ? <></>
+    : (
+      <Table striped bordered size='sm'>
+        <thead>
+          <tr>
+            <th>Weapon</th>
+            {props.phase === 'shooting' && <th>Range</th>}
+            {props.phase === 'shooting' && <th>Type</th>}
+            {props.phase === 'shooting' && <th>Shots</th>}
+            <th>S</th>
+            <th>AP</th>
+            <th>D</th>
+            <th>Abilities</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
-  )
+        </thead>
+        <tbody>
+          {_.sortBy(weapons, ['name']).map((x: Weapon) => (
+            <tr key={x.id}>
+              <td style={{ whiteSpace: 'nowrap' }}>{x.name}</td>
+              {props.phase === 'shooting' && <td>{x.range}&quot;</td>}
+              {props.phase === 'shooting' && <td style={{ whiteSpace: 'nowrap' }}>{x.type}</td>}
+              {props.phase === 'shooting' && <td style={{ whiteSpace: 'nowrap' }}>{x.shots}</td>}
+              <td>{x.strength}</td>
+              <td>{x.armourPiercing}</td>
+              <td>{x.damage}</td>
+              <td>{x.abilities}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      )
 }
 
 export default WeaponList
--- src/components/KillTeam2021/AbilityList.tsx
+++ src/components/KillTeam2021/AbilityList.tsx
@@ -9,21 +9,23 @@ interface Props {
 }
 
 function AbilityList (props: Props): JSX.Element {
-  return props.abilities.length === 0 ? <></> : (
-    <Card border='secondary' bg='light'>
-      <Card.Header style={{ background: 'rgba(0, 0, 0, 0.05)' }} as='h4'>
-        Abilities
-      </Card.Header>
-      <Card.Body>
-        {_.sortBy(props.abilities, ['name']).map((x: Ability) => (
-          <p key={x.id}>
-            <strong>{x.name}: </strong>
-            <CompileDescription>{x.description}</CompileDescription>
-          </p>
-        ))}
-      </Card.Body>
-    </Card>
-  )
+  return props.abilities.length === 0
+    ? <></>
+    : (
+      <Card border='secondary' bg='light'>
+        <Card.Header style={{ background: 'rgba(0, 0, 0, 0.05)' }} as='h4'>
+          Abilities
+        </Card.Header>
+        <Card.Body>
+          {_.sortBy(props.abilities, ['name']).map((x: Ability) => (
+            <p key={x.id}>
+              <strong>{x.name}: </strong>
+              <CompileDescription>{x.description}</CompileDescription>
+            </p>
+          ))}
+        </Card.Body>
+      </Card>
+      )
 }
 
 export default AbilityList
