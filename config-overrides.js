921e39f44f61a491386666cdff776d33c0f71e77
James Smith <james@floppy.org.uk>
2022-02-13 12:07:14 +0000

    add crypto-browserify override

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,13 @@
 const webpack = require('webpack');
 module.exports = function override(config, env) {
     config.resolve.fallback = {
+        crypto: require.resolve('crypto-browserify'),
     };
     config.plugins.push(
+        // new webpack.ProvidePlugin({
+        //     process: 'process/browser',
+        //     Buffer: ['buffer', 'Buffer'],
+        // }),
     );
 
     return config;
