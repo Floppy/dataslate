64522e82cf5e09ca52100468ca739f8ce0184d9a
James Smith <james@floppy.org.uk>
2022-02-13 12:18:40 +0000

    Add stream override

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ module.exports = function override(config, env) {
     config.resolve.fallback = {
         crypto: require.resolve('crypto-browserify'),
         buffer: require.resolve('buffer'),
+        stream: require.resolve('stream-browserify'),
     };
     config.plugins.push(
         // new webpack.ProvidePlugin({
