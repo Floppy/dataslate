58d229c81da6b1bfe5f4513a18ad936b617db857
James Smith <james@floppy.org.uk>
2022-02-23 22:37:39 +0000

    add skeleton for 40k support

--- src/FileLoader.ts
+++ src/FileLoader.ts
@@ -1,10 +1,12 @@
 import { parseBattlescribeXML as parseKillTeam2018 } from './parsers/KillTeam2018/BattlescribeParser'
 import { parseBattlescribeXML as parseKillTeam2021 } from './parsers/KillTeam2021/BattlescribeParser'
+import { parseBattlescribeXML as parse40k9e } from './parsers/WH40k9e/BattlescribeParser'
 import JSZip from 'jszip'
 import { DOMParserImpl } from 'xmldom-ts'
 import * as XPath from 'xpath-ts'
 import { Roster as Roster2018 } from './types/KillTeam2018'
 import { Roster as Roster2021 } from './types/KillTeam2021'
+import { Roster as Roster40k9e } from './types/WH40k9e'
 
 // useNamespaces is NOT a React hook, so:
 // eslint-disable-next-line
@@ -20,7 +22,7 @@ const unzip = async (file: string): Promise<string> => {
   }
 }
 
-const parseFile = (file: string): Roster2018 | Roster2021 => {
+const parseFile = (file: string): Roster2018 | Roster2021 | Roster40k9e => {
   const doc = new DOMParserImpl().parseFromString(file)
   const gameSystemId = (xpSelect('/bs:roster/@gameSystemId', doc, true) as Node).nodeValue
   switch (gameSystemId) {
@@ -28,12 +30,14 @@ const parseFile = (file: string): Roster2018 | Roster2021 => {
       return parseKillTeam2018(doc) as Roster2018
     case '3b7e-7dab-f79f-2e74':
       return parseKillTeam2021(doc)
+    case '28ec-711c-d87f-3aeb':
+      return parse40k9e(doc)
     default:
       throw new Error('Unsupported game system!')
   }
 }
 
-export const loadFile = async (file: File): Promise<Roster2018 | Roster2021> => {
+export const loadFile = async (file: File): Promise<Roster2018 | Roster2021 | Roster40k9e> => {
   const reader = new FileReader()
   return await new Promise((resolve, reject) => {
     reader.onerror = () => {
@@ -49,6 +53,6 @@ export const loadFile = async (file: File): Promise<Roster2018 | Roster2021> => 
   })
 }
 
-export const loadFiles = async (files: File[]): Promise<Roster2018 | Roster2021> => {
+export const loadFiles = async (files: File[]): Promise<Roster2018 | Roster2021 | Roster40k9e> => {
   return await loadFile(files[0])
 }
