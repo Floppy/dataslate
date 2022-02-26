912a5c8c737bf5fb21229a8527052caf6c525a7d
James Smith <james@floppy.org.uk>
2022-02-26 17:36:10 +0000

    lint fixes

--- src/components/WH40k9e/StratagemList.tsx
+++ src/components/WH40k9e/StratagemList.tsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import { Card, CardColumns, Badge } from "react-bootstrap";
-import { usePapaParse } from 'react-papaparse';
-
-interface Props {
-}
+import { Card, CardColumns, Badge } from 'react-bootstrap'
+import { usePapaParse } from 'react-papaparse'
 
 const headingStyle = {
   background: '#FF6F2D',
@@ -13,7 +10,7 @@ const headingStyle = {
   display: 'flex'
 }
 
-const getData=(setStratagems: any)=>{
+const getData = (setStratagems: any): void => {
   // eslint-disable-next-line
   const { readRemoteFile } = usePapaParse();
   const url = '/waha/wh40k9ed/Stratagems.csv'
@@ -24,33 +21,35 @@ const getData=(setStratagems: any)=>{
     fastMode: true,
     complete: (results) => {
       setStratagems(results.data)
-    },
+    }
   })
 }
 
-export function StratagemList (props: Props): JSX.Element {
-  let [stratagems, setStratagems] = useState([])
-
+export function StratagemList (): JSX.Element {
+  const [stratagems, setStratagems] = useState([])
   getData(setStratagems)
-  return <Card>
-    <Card.Header style={{ ...headingStyle }} as='h2'>Stratagems</Card.Header>
-    <Card.Body>
-      <CardColumns>
-        { stratagems.filter((stratagem) => (stratagem.faction_id === 'TYR')).map((stratagem) => (
-          <Card border='secondary' bg='light'>
-            <Card.Header style={{ background: 'rgba(0, 0, 0, 0.05)' }} as='h4'>
-              {stratagem.name} <Badge variant="primary" className="float-right">{stratagem.cp_cost} CP</Badge>
-            </Card.Header>
-            <Card.Body>
-              <p><strong>{stratagem.type}</strong></p>
-              <p dangerouslySetInnerHTML={{
-                __html: stratagem.description
-              }}></p>
-            </Card.Body>
-          </Card>
-        ))}
-      </CardColumns>
-    </Card.Body>
-    <Card.Footer>Powered by Wahapedia</Card.Footer>
-  </Card>
+  return (
+    <Card>
+      <Card.Header style={{ ...headingStyle }} as='h2'>Stratagems</Card.Header>
+      <Card.Body>
+        <CardColumns>
+          {stratagems.filter((stratagem) => (stratagem.faction_id === 'TYR')).map((stratagem) => (
+            <Card key={stratagem.id} border='secondary' bg='light'>
+              <Card.Header style={{ background: 'rgba(0, 0, 0, 0.05)' }} as='h4'>
+                {stratagem.name} <Badge variant='primary' className='float-right'>{stratagem.cp_cost} CP</Badge>
+              </Card.Header>
+              <Card.Body>
+                <p><strong>{stratagem.type}</strong></p>
+                <p dangerouslySetInnerHTML={{
+                  __html: stratagem.description
+                }}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </CardColumns>
+      </Card.Body>
+      <Card.Footer>Powered by Wahapedia</Card.Footer>
+    </Card>
+  )
 }
