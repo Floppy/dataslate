ef4acd168d58fc8e8214a843b74b059d4a3fc5cc
G3rg <mauve.simian@gmail.com>
2022-06-08 13:42:11 +1000

    Blooded tactical ploys done

--- src/components/KillTeam2021/data/blooded.ts
+++ src/components/KillTeam2021/data/blooded.ts
@@ -8,24 +8,66 @@ const archetypes: FireteamArchetypes = {
 
 const strategicPloys: Ploy[] = [
   {
-    name: 'A',
+    name: 'Overcharge Lasguns',
     cost: 1,
-    description: `**PHOBOS STRIKE TEAM💀**`
+    description: `Until the end of the Turning Point, each time a las weapon is selected for a friendly **BLOODED💀** operative to shoot with
+    (excluding a Sharpshooter's long-las), you can choose to overcharge it. If you do so, that weapon gains the AP1 and Hot special rules for 
+    that shooting attack. A las weapon is a ranged weapon that includes 'las' in its name, e.g. lasgun, laspistol etc.`
+  },
+  {
+    name: 'Glory Kill',
+    cost: 1,
+    description: `Select one enemy operative Visible to a friendly **BLOODED💀** operative. Until the end of the Turning Point, each time a friendly
+    **BLOODED💀** operative fights in combat with or makes a shooting attack against that enemy operative, in the Roll Attack Dice step of that
+    combat or shooting attack, you can re-roll one of your attack dice.`
+  },
+  {
+    name: 'Reckless Aspirants',
+    cost: 1,
+    description: `In the next Firefight phase, while a friendly **BLOODED💀** operative that does not have a Blooded token is within ⬟ of your
+    opponent's drop zone, treat it as if it has a Blooded token.`
+  },
+  {
+    name: 'Dirty Fighters',
+    cost: 1,
+    description: `Until the end of the Turning Point, each time a friendly **BLOODED💀** operative fights in combat, in the Roll Attack Dice step
+    of that combat, if another friendly operative is supporting them, you can retain one of your successful normal hits as a critical hit (excluding
+    a normal hit retained as a result of a Blooded token).`
   }
 ]
 
 const tacticalPloys: Ploy[] = [
   {
-    name: 'A',
+    name: 'Callous Disregard',
     cost: 1,
-    description: `Use this Tactical Ploy when a friendly **PHOBOS STRIKE TEAM💀** operative with a Conceal order that is not within Engagement Range of an
-    enemy is activated.
-  * That operative cannot make a shooting attack during that activation.
-  * The first time that operative performs a **Fight** action during that activation, in the Resolve Successful Hits step of that combat, the first time
-  you resolve one of your successful hits, you can immediately resolve another of your successful hits.
-   
-  **Designer's Note:** Unless the operative can perform a **Charge** action while it has a Conceal order (e.g. The Assassin Battle Honour), when it is activated,
-  you will need to change its order to Engage to use this Tactical Ploy.`
+    description: `Use this Tactical Ploy when a **Shoot** action is declared for a friendly **BLOODED💀** operative. For that shooting attack:
+  * Having other friendly operatives within Engagement Range of an enemy operative does not prevent that enemy operative from being selected as a valid target.
+  * When determining Line of Sight, enemy operatives cannot use friendly operatives' bases as Cover.
+  * In the Roll Attack Dice step of that shooting attack, failed hits are instead retained separately as successful normal hits.
+  * In the Resolve Successful Hits step of that shooting attack, those retained hits inflict damage on one friendly operative within Engagement Range of the 
+  target operative.`
+  },
+  {
+    name: 'Moment of Repute',
+    cost: 1,
+    description: `Use this Tactical Ploy when a friendly **BLOODED💀** operative that is under the gaze of the gods is activated. Add 1 to its APL.`
+  },
+  {
+    name: 'Reward Earned',
+    cost: 1,
+    description: `Use this Tactical Ploy when an enemy operative is incapacitated by a friendly **BLOODED💀** operative within ■ of it. Add 1 Blooded 
+    token to your pool.`
+  },
+  {
+    name: 'Dark Favour',
+    cost: 1,
+    description: `Use this Tactical Ploy when a friendly **BLOODED💀** operative that has a Blooded token is slected as the target of a shooting attack.
+    Select one other friendly **BLOODED💀** operative that:
+  * Does not have a Blooded token.
+  * Is within ⬤ of that friendly operative.
+  * Is Visible to the active operative.
+  
+  Resolve that shooting attack against that other friendly operative instead (it is treated as a valid target).`
   }
 ]
 
