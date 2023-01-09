c4a077af4046dbd60efcbe036d101c028e7650fb
James Smith <james@floppy.org.uk>
2023-01-09 21:29:08 +0000

    reformat fight phase layout

--- src/components/WH40k9e/FightPhaseDetails.tsx
+++ src/components/WH40k9e/FightPhaseDetails.tsx
@@ -15,31 +15,39 @@ export function FightPhaseDetails (props: Props): JSX.Element {
   return (
     <>
       <Row>
-        <Col sm='12'>
+        <Col sm='10'>
           <UnitTitle name={props.unit.name} uuid={props.unit.id} datasheet={props.unit.datasheet} />
+          {
+            props.unit.profiles.length > 1
+            ? props.unit.profiles.map((profile: Profile) => (
+              <ProfileTitle name={profile.name} />
+            ))
+            : <></>
+          }
+          <WeaponList weapons={props.unit.weapons} phase='fight' />
+          <AbilityList abilities={props.unit.abilities} phase='fight' />
+        </Col>
+        <Col sm='1' className='px-1'>
+          {props.unit.profiles.map((profile: Profile) => (
+            <>
+              <StatBadge name='A' value={`${profile.profileStats.attacks}`} />
+              <StatBadge name='WS' value={`${profile.profileStats.weapon_skill}`} secondaryValue='+' />
+              <StatBadge name='S' value={`${profile.profileStats.strength}`} />
+            </>
+          ))}
+        </Col>
+        <Col sm='1' className='px-1'>
+          {props.unit.profiles.map((profile: Profile) => (
+            <>
+              <StatBadge name='T' value={`${profile.profileStats.toughness}`} />
+              <StatBadge name='Sv' value={`${profile.profileStats.save}`} secondaryValue='+' />
+              {profile.profileStats.invulnerable_save > 0
+                ? <StatBadge name='Inv' value={`${profile.profileStats.invulnerable_save}`} secondaryValue='++' />
+                : <></>}
+            </>
+          ))}
         </Col>
       </Row>
-      {props.unit.profiles.map((profile: Profile) => (
-        <Row key={profile.id}>
-          <Col sm='10'>
-            <ProfileTitle name={profile.name} />
-          </Col>
-          <Col sm='1' className='px-1'>
-            <StatBadge name='A' value={`${profile.profileStats.attacks}`} />
-            <StatBadge name='T' value={`${profile.profileStats.toughness}`} />
-            <StatBadge name='S' value={`${profile.profileStats.strength}`} />
-          </Col>
-          <Col sm='1' className='px-1'>
-            <StatBadge name='WS' value={`${profile.profileStats.weapon_skill}`} secondaryValue='+' />
-            <StatBadge name='Sv' value={`${profile.profileStats.save}`} secondaryValue='+' />
-            {profile.profileStats.invulnerable_save > 0
-              ? <StatBadge name='Inv' value={`${profile.profileStats.invulnerable_save}`} secondaryValue='++' />
-              : <></>}
-          </Col>
-        </Row>
-      ))}
-      <WeaponList weapons={props.unit.weapons} phase='fight' />
-      <AbilityList abilities={props.unit.abilities} phase='fight' />
     </>
   )
 }
