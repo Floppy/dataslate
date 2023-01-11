1f174e7b0c7bb93158131c534213e534a51f82e2
James Smith <james@floppy.org.uk>
2023-01-11 15:46:35 +0000

    fix a couple of phase mappings in stratagem loader

--- src/parsers/WH40k9e/StratagemDownloaderPlugin.js
+++ src/parsers/WH40k9e/StratagemDownloaderPlugin.js
@@ -4,28 +4,34 @@ const fs = require('fs')
 const parse = require('csv-parse/sync').parse
 const slugify = require('slugify')
 
+const getFile = async (filename) => {
+  const csvOptions = {
+    columns: true,
+    delimiter: '|',
+    quote: null,
+    skip_empty_lines: true
+  }
+  console.log(`Downloading ${filename}`)
+  const file = await axios.get(`https://wahapedia.ru/wh40k9ed/${filename}.csv`)
+  return parse(file.data, csvOptions)
+}
+
 class StratagemDownloaderPlugin {
   apply (compiler) {
     compiler.hooks.afterResolvers.tap('StratagemDownloaderPlugin', async () => {
-      const csvOptions = {
-        columns: true,
-        delimiter: '|',
-        quote: null,
-        skip_empty_lines: true
-      }
       const phaseMapping = {
         'Any of your phases': [],
         'Any phase': [],
         'Any time': [],
         'At the start of battle round': ['battle_round_start'],
-        'At the start of enemy turn': ['battle_round_start'],
-        'At the start of your turn': ['battle_round_start'],
-        'Before battle': [],
+        'At the start of enemy turn': ['command'],
+        'At the start of your turn': ['command'],
+        'Before battle': ['before-battle'],
         'Being targeted': ['fight', 'shooting'],
         'Charge phase': ['charge'],
         'Command phase': ['command'],
         'During deployment': ['deployment'],
-        'End of your turn': [''],
+        'End of your turn': ['end-of-turn'],
         'Enemy Charge phase': ['charge'],
         'Enemy Command phase': ['command'],
         'Enemy Fight phase': ['fight'],
@@ -42,18 +48,14 @@ class StratagemDownloaderPlugin {
         'Taking casualties': ['fight', 'shooting']
       }
       try {
-        console.log('Downloading stratagem data')
-        const stratagemFile = await axios.get('https://wahapedia.ru/wh40k9ed/Stratagems.csv')
-        const stratagemData = parse(stratagemFile.data, csvOptions)
-        console.log('Downloading stratagem->phase mapping')
-        const stratagemPhaseFile = await axios.get('https://wahapedia.ru/wh40k9ed/StratagemPhases.csv')
-        const stratagemPhaseData = parse(stratagemPhaseFile.data, csvOptions)
-        console.log('Downloading datasheet data')
-        const datasheetFile = await axios.get('https://wahapedia.ru/wh40k9ed/Datasheets.csv')
-        const datasheetData = parse(datasheetFile.data, csvOptions)
-        console.log('Downloading datasheet->stratagem mapping')
-        const datasheetStratagemFile = await axios.get('https://wahapedia.ru/wh40k9ed/Datasheets_stratagems.csv')
-        const datasheetStratagemData = parse(datasheetStratagemFile.data, csvOptions)
+        // Fetch all the data
+        const stratagemData = await getFile('Stratagems')
+        const stratagemPhaseData = await getFile('StratagemPhases')
+        const datasheetData = await getFile('Datasheets')
+        const datasheetStratagemData = await getFile('Datasheets_stratagems')
+        const factionData = await getFile('Factions')
+        const sourceData = await getFile('Source')
+        // Convert into our structures
         console.log('Building stratagems')
         const stratagems = stratagemData.map((data) => ({
           id: data.id,
@@ -78,6 +80,7 @@ class StratagemDownloaderPlugin {
         console.log('Writing stratagem code')
         fs.writeFile('src/parsers/WH40k9e/Stratagems.ts', `import { Stratagem } from '../../types/WH40k9e'
           export const stratagems: Stratagem[] = ${JSON.stringify(stratagems, null, 2)}`, () => true)
+        console.log('Stratagems complete!')
       } catch (error) {
         console.error(error)
       }
